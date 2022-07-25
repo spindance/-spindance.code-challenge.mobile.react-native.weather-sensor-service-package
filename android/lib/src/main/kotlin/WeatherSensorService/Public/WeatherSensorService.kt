b49089c4af5b11754eaea033337fb0c51e9bcb48
ClaireL-spindance <84326777+ClaireL-spindance@users.noreply.github.com>
2022-07-25 14:29:38 -0400

    overall project structure

--- android/lib/src/main/kotlin/WeatherSensorService/Public/WeatherSensorService.kt
+++ android/lib/src/main/kotlin/WeatherSensorService/Public/WeatherSensorService.kt
@@ -5,6 +5,8 @@
 // Copyright © 2022 SpinDance. All rights reserved.
 //
 
+import com.facebook.react.bridge.*
+
 public object WeatherSensorService {
     public val reader: WeatherSensorReaderType = MockWeatherSensorReader()
 }
